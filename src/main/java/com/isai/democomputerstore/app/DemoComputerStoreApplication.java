d1bb5374ba8c3257863f3f60389f73366e168631
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-06 08:47:51 -0500

    init

---
+++ src/main/java/com/isai/democomputerstore/app/DemoComputerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.isai.democomputerstore.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoComputerStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoComputerStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/democomputerstore/app/DemoComputerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.democomputerstore.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoComputerStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
