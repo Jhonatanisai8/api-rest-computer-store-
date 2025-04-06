5d9742d5b7f1c2bc86193813578723132e3ea107
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-06 10:33:23 -0500

    enum con algunos errores con su codido de error y ademas una breve descripcion

---
+++ src/main/java/com/isai/democomputerstore/app/utils/ErrorCatalog.java
@@ -0,0 +1,20 @@
+package com.isai.democomputerstore.app.utils;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCatalog {
+
+    PRODUCT_NOT_FOUND("ERR_PROD_001", "Product not found"),
+    INVALID_PRODUCT("ERR_PROD002", "Invalid product parameters"),
+    MAKER_NOT_FOUND("ERR_MAKER001", "Maker not found"),
+    GENERIC_ERROR("ERR_GEN001", "An unexpected error occurred");
+
+    private final String codeError;
+    private final String messageError;
+
+    private ErrorCatalog(String codeError, String messageError) {
+        this.codeError = codeError;
+        this.messageError = messageError;
+    }
+}
