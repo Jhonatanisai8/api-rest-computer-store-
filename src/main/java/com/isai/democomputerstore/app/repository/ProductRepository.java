2b0e3932d8b7d8ea095dd73d57af5250e1e55e73
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-06 09:32:41 -0500

    repositorio de producto: definicion del metodo para buscar los productos por un determinado fabricante

--- src/main/java/com/isai/democomputerstore/app/repository/ProductRepository.java
+++ src/main/java/com/isai/democomputerstore/app/repository/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.isai.democomputerstore.app.repository;
 
+import com.isai.democomputerstore.app.models.entitys.Maker;
 import com.isai.democomputerstore.app.models.entitys.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository
         extends JpaRepository<Product, Integer> {
+
+    List<Product> findAllByMaker(Maker maker);
 }
